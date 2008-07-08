5a49038227b5b2d5e65288851593809217a21661
oleg dashevskii <be9@be9.ru>
2008-07-08 11:54:16 +0700

    Moved errors back to test_parse.py

--- assembler/test/test_parse.py
+++ assembler/test/test_parse.py
@@ -20,15 +20,26 @@ empties = """
 * *
 *+*"""
 
+errors = \
+"""	ENTU	100 comment
+label	EQUU	5
+ * bad comment
+	* * bad comment
+VERYLONGLABEL ENTA	5
+6546	ENTA	3
+label*	ENTA	3
+%	ENTA	3
+#	ENTA	3
+label	EQUUUU"""
+
 class ParserTestCase(unittest.TestCase):
   def testEmptyLine(self):
     for line in empties.split("\n"):
       self.assertEqual(parse_line(line), None)
 
   def testErrors(self):
-    with open("test/errors.dat","r") as file:
-      for line in file:
-        self.assertRaises(AssemblySyntaxError, parse_line, line)
+    for line in errors.split("\n"):
+      self.assertRaises(AssemblySyntaxError, parse_line, line)
 
 suite = unittest.makeSuite(ParserTestCase, 'test')
 
