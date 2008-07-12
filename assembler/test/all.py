863e61271adf0afc2cbd198cf49c1775b328e5f8
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-07-12 23:54:35 +0700

    Memory test added to all tests

--- assembler/test/all.py
+++ assembler/test/all.py
@@ -6,8 +6,9 @@ import unittest
 import test_parse_line
 import test_parse_lines
 import test_symbol_table
+import test_memory
 
 def suite():
-  return unittest.TestSuite((test_parse_line.suite, test_parse_lines.suite, test_symbol_table.suite))
+  return unittest.TestSuite((test_parse_line.suite, test_parse_lines.suite, test_symbol_table.suite, test_memory.suite))
 
 unittest.TextTestRunner().run(suite())
