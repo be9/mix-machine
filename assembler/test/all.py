c221727b1f08c140a12e7e10b4966260d60f925c
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-07-13 18:25:18 +0700

    test_assemble added to all tests

--- assembler/test/all.py
+++ assembler/test/all.py
@@ -9,9 +9,10 @@ import test_parse_lines
 import test_symbol_table
 import test_memory
 import test_operations
+import test_assemble
 
 def suite():
   return unittest.TestSuite((test_parse_line.suite, test_parse_lines.suite, test_symbol_table.suite, \
-    test_memory.suite, test_operations.suite, test_parse_argument.suite))
+    test_memory.suite, test_operations.suite, test_parse_argument.suite, test_assemble.suite))
 
 unittest.TextTestRunner().run(suite())
