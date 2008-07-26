0fad19ced8bdcb0683787b78e234c6319555a6a7
Alexey Kuznecov <kuznecov.alexey@gmail.com>
2008-07-26 19:34:38 +0700

    Cleaned some dead code

--- vm/vm_command_store.py
+++ vm/vm_command_store.py
@@ -2,62 +2,19 @@ from vm_word import Word
 from vm_command import cmdList
 
 def sta(command, context):
-#	print "--[store]--"
-#	data = context.mem.get(command["M"])
-#	num = command["F"][1] - command["F"][0] + 1
-#	
-#	if command["F"][0] == 0:
-#		bytes = Word(context.regs["A"]).shift_r(num-1)
-#		data.set_bytes(bytes, (1, command["F"][1]))
-#	else:
-#		bytes = Word(context.regs["A"]).shift_r(num)
-#	print bytes
-#	
-#	data.set_bytes(bytes, command["F"])
-#	
-#	print data
-#	
-#	context.mem.set(data, command["M"])
-#	print "-----------"
+	pass
 
 def stx(command, context):
-	data = context.mem.get(command["M"])
-	
-	bytes = context.regs["X"].get_bytes((5-F[1]+F[0],5))
-	data.set_bytes(bytes, command["F"])
-	
-	context.mem.set(data, command["M"])
-	
+	pass
+
 def sti(command, context):
-	addr = command.addr() + context.get_reg_index(command.index()).int()
-	word = context.mem.get(addr)
-	F = divmod(command.fmt(), 8)
-	
-	bytes = context.get_reg_index(command.code() - 24)
-	bytes.shift_l(3)
-	word.set_bytes(bytes.get_bytes((2-F[1]+F[0], 2)), F)
-	
-	context.mem.set(word, addr)
-	
-	return context.get_reg_l().int() + 1
+	pass
 
 def stj(command, context):
-	addr = command.addr() + context.get_reg_index(command.index()).int()
-	word = context.mem.get(addr)
-	F = divmod(command.fmt(), 8)
-	
-	bytes = context.regs["J"]
-	bytes.shift_l(3)
-	word.set_bytes(bytes.get_bytes((2-F[1]+F[0], 2)), F)
-	
-	context.mem.set(word, addr)
-	
-	return context.get_reg_l().int() + 1
+	pass
 
 def stz(command, context):
-	addr = command.addr() + context.get_reg_index(command.index()).int()
-	context.mem.set(Word(0), addr)
-	return context.get_reg_l().int() + 1
+	pass
 
 cmdList.add_command(24,	-1, sta, 1,	"STA")
 cmdList.add_command(31,	-1, stx, 1,	"STX")
