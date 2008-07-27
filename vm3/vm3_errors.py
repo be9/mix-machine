3b0cef6a69dc6bd2eba8190330b2d00b9c19d458
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-07-28 01:44:01 +0700

    Errors for vm3. Can be modified...

---
+++ vm3/vm3_errors.py
@@ -0,0 +1,17 @@
+class InvalidAddress(Exception):
+  pass
+
+class InvalidIndex(Exception):
+  pass
+
+class InvalidFieldSpec(Exception):
+  pass
+
+class UnknownInstruction(Exception):
+  pass
+
+class InvalidCA(Exception):
+  pass
+
+class NegativeShift(Exception):
+  pass
