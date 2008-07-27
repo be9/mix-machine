e8e0269fbe0ed2aa13d1efd5e893946816d07f09
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-07-27 13:47:04 +0700

    Removed blahTest :)

--- vm2/test/test_execution.py
+++ vm2/test/test_execution.py
@@ -5,19 +5,7 @@ from execution import *
 from errors import *
 
 class ExecutionTestCase(unittest.TestCase):
-  def blahTestFinder(self):
-    array = [0, 10, 20, 25]
-    tests = [
-      (0, (0, True)),
-      (25, (25, True)),
-      (10, (10, True)),
-      (5, (0, False)),
-      (9, (0, False)),
-      (15, (10, False)),
-      (26, (25, False)),
-    ]
-    for value, result in tests:
-      self.assertEqual(find_nearest_down(array, value), result)
+  pass
 
 suite = unittest.makeSuite(ExecutionTestCase, 'test')
 
