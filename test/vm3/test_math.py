afb9355c42de0ab95c426906e163693b352e33c2
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-07-29 02:58:59 +0700

    vm3: sub tests

--- test/vm3/test_math.py
+++ test/vm3/test_math.py
@@ -2,11 +2,17 @@ import unittest
 from basetestcase import *
 
 class VM3MathTestCase(VM3BaseTestCase):
-  def testADD(self):
+  def testADDandSUB(self):
     self.check1(
       regs = {'A': [+1, 0, 0, 0, 0, 0]},
       memory = {0: [+1, 0, 0, 0, 5, 1]},
       diff = {'CA': 1, 'A': [+1, 0, 0, 0, 5, 1]},
+      cycles = 2
+    )
+    self.check1(
+      regs = {'A': [+1, 0, 0, 0, 0, 0]},
+      memory = {0: [+1, 0, 0, 0, 5, 2]},
+      diff = {'CA': 1, 'A': [-1, 0, 0, 0, 5, 2]},
       cycles = 2
     )
 
@@ -23,6 +29,12 @@ class VM3MathTestCase(VM3BaseTestCase):
       diff = {'CA': 1, 'A': [+1, 5, 4, 3, 2, 29]},
       cycles = 2
     )
+    self.check1(
+      regs = {'A': [+1, 5, 4, 3, 2, 1]},
+      memory = {0: [+1, 0, 0, 0,28, 2]}, # 28 = (3:4)
+      diff = {'CA': 1, 'A': [+1, 5, 4, 3, 1, 37]},
+      cycles = 2
+    )
 
     self.check1(
       regs = {'A': [+1, 5, 4, 3, 2, 1]},
@@ -31,12 +43,30 @@ class VM3MathTestCase(VM3BaseTestCase):
       cycles = 2,
       message = "adding zero-part of word"
     )
+    self.check1(
+      regs = {'A': [+1, 5, 4, 3, 2, 1]},
+      memory = {0: [+1, 0, 0, 0, 2, 2]}, # 2 = (0:2)
+      diff = {'CA': 1},
+      cycles = 2,
+      message = "subtracting zero-part of word"
+    )
 
     self.check1(
       regs = {'A': [-1, 63, 2, 0, 0, 3]},
       memory = {
         0  : [+1, 63, 2, 0, 0, 3],
         10 : [+1, 0, 0, 0, 5, 1]
+      },
+      startadr = 10,
+      diff = {'CA': 11, 'A': [-1, 0, 0, 0, 0, 0]},
+      cycles = 2,
+      message = "saving sign of rA, if rA == 0"
+    )
+    self.check1(
+      regs = {'A': [-1, 63, 2, 0, 0, 3]},
+      memory = {
+        0  : [-1, 63, 2, 0, 0, 3],
+        10 : [+1, 0, 0, 0, 5, 2]
       },
       startadr = 10,
       diff = {'CA': 11, 'A': [-1, 0, 0, 0, 0, 0]},
@@ -55,12 +85,34 @@ class VM3MathTestCase(VM3BaseTestCase):
       cycles = 2,
       message = "saving sign of rA, if rA == 0"
     )
+    self.check1(
+      regs = {'A': [+1, 63, 2, 0, 0, 3]},
+      memory = {
+        0  : [+1, 63, 2, 0, 0, 3],
+        10 : [+1, 0, 0, 0, 5, 2]
+      },
+      startadr = 10,
+      diff = {'CA': 11, 'A': [+1, 0, 0, 0, 0, 0]},
+      cycles = 2,
+      message = "saving sign of rA, if rA == 0"
+    )
 
     self.check1(
       regs = {'A': [+1, 63, 60, 60, 60, 60]},
       memory = {
         0  : [+1, 2, 3, 2, 1, 4],
         10 : [+1, 0, 0, 0, 5, 1]
+      },
+      startadr = 10,
+      diff = {'CA': 11, 'OF' : 1, 'A': [+1, 1, 63, 62, 62, 0]},
+      cycles = 2,
+      message = "overflow test"
+    )
+    self.check1(
+      regs = {'A': [+1, 63, 60, 60, 60, 60]},
+      memory = {
+        0  : [-1, 2, 3, 2, 1, 4],
+        10 : [+1, 0, 0, 0, 5, 2]
       },
       startadr = 10,
       diff = {'CA': 11, 'OF' : 1, 'A': [+1, 1, 63, 62, 62, 0]},
@@ -85,6 +137,12 @@ class VM3MathTestCase(VM3BaseTestCase):
     self.assertRaises(InvalidFieldSpec, self.check1,
       regs = {'A': [+1, 5, 4, 3, 2, 1]},
       memory = {0: [+1, 0, 0, 0,26, 1]}, # 26 = (3:2)
+      diff = {'CA': 1, 'A': [+1, 5, 4, 3, 2, 29]},
+      cycles = 2
+    )
+    self.assertRaises(InvalidFieldSpec, self.check1,
+      regs = {'A': [+1, 5, 4, 3, 2, 1]},
+      memory = {0: [+1, 0, 0, 0,26, 2]}, # 26 = (3:2)
       diff = {'CA': 1, 'A': [+1, 5, 4, 3, 2, 29]},
       cycles = 2
     )
