94d628a5b925646b0fea163bb786b7334a573bd2
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-07-29 17:42:50 +0700

    vm3: all LOAD tests done

--- test/vm3/test_load.py
+++ test/vm3/test_load.py
@@ -2,7 +2,7 @@ import unittest
 from basetestcase import *
 
 class VM3LoadTestCase(VM3BaseTestCase):
-  def test(self):
+  def testNormal(self):
     self.check1(
       memory = {
         10: [+1, 0, 50, 0, 5, 8],
@@ -78,7 +78,78 @@ class VM3LoadTestCase(VM3BaseTestCase):
       cycles = 2
     )
 
-    
+    self.check1(
+      memory = {
+        10: [+1, 0, 50, 0, 20, 15], # 2:4
+        50: [-1, 0, 2, 5, 4, 1]
+      },
+      startadr = 10,
+      diff = {
+        'CA' : 11,
+        'X' : [+1, 0, 0, 2, 5, 4]
+      },
+      cycles = 2
+    )
+    self.check1(
+      memory = {
+        10: [+1, 0, 50, 0, 20, 23], # 2:4
+        50: [-1, 0, 2, 5, 4, 1]
+      },
+      startadr = 10,
+      diff = {
+        'CA' : 11,
+        'X' : [-1, 0, 0, 2, 5, 4]
+      },
+      cycles = 2
+    )
+
+    for i in xrange(1, 6+1):
+      self.check1(
+        memory = {
+          10: [+1, 0, 50, 0, 20, 8+i], # 2:4
+          50: [-1, 0, 2, 5, 4, 1]
+        },
+        startadr = 10,
+        diff = {
+          'CA' : 11,
+          'I'+str(i) : [+1, 0, 0, 0, 5, 4]
+        },
+        cycles = 2
+      )
+      self.check1(
+        memory = {
+          10: [+1, 0, 50, 0, 20, 16+i], # 2:4
+          50: [-1, 0, 2, 5, 4, 1]
+        },
+        startadr = 10,
+        diff = {
+          'CA' : 11,
+          'I'+str(i) : [-1, 0, 0, 0, 5, 4]
+        },
+        cycles = 2
+      )
+
+  def testRaises(self):
+    for c_code in xrange(8, 23+1):
+      self.assertRaises(InvalidAddress, self.exec1,
+        memory = {
+          10 : [+1, 63, 63, 0, 5, c_code]
+        },
+        startadr = 10,
+      )
+      self.assertRaises(InvalidIndex, self.exec1,
+        memory = {
+          10 : [+1, 63, 63, 44, 5, c_code]
+        },
+        startadr = 10,
+      )
+      self.assertRaises(InvalidFieldSpec, self.exec1,
+        memory = {
+          10 : [+1, 3, 63, 0, 8, c_code] # 8 = 1:0
+        },
+        startadr = 10,
+      )
+
 
 suite = unittest.makeSuite(VM3LoadTestCase, 'test')
 
