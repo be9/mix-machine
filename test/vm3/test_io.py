62be47881cdedc2561dc9629b52570215904531e
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-08-02 01:38:50 +0700

    vm3: added tests for OUT and IOC for outputting-device

--- test/vm3/test_io.py
+++ test/vm3/test_io.py
@@ -2,9 +2,8 @@ import unittest
 from basetestcase import *
 
 class VM3IOTestCase(VM3BaseTestCase):
-  def test(self):
+  def testOUT(self):
     out_file = open("18.dev", "w")
-    #in_file =  open("19.dev", "r")
     self.check_hlt(
       regs = {
         'W_LOCKED' : set([2000, 2001, 2002]),
@@ -37,11 +36,10 @@ class VM3IOTestCase(VM3BaseTestCase):
         148 : [+1, 0, 0, 0, 0, 0],
         149 : [+1, 0, 0, 0, 0, 0],
         150 : [+1, 0, 0, 0, 0, 0],
-        151 : [+1, 0, 0, 0, 0, 0],
+        151 : [+1, 0, 0, 0, 0, 10],
       },
       devs = {
-        18 : (0, 'w', 24*5, 24*2, out_file),
-        #19 : (0, 'r', 14*5, 14*2, in_file)
+        18 : (0, 'w', 24*5, 24*2, out_file)
       },
       diff = {
         'CA' : 3,
@@ -52,8 +50,172 @@ class VM3IOTestCase(VM3BaseTestCase):
     )
     out_file.close()
     out_file = open("18.dev", "r") 
-    self.assertEqual(out_file.read(), " ABCDEFGHI~JKLMNOPQR[#STUVWXYZ0123456789.,()+-*/=$<>@;:'"+" "*(120-56))
+    self.assertEqual(out_file.read(), " ABCDEFGHI~JKLMNOPQR[#STUVWXYZ0123456789.,()+-*/=$<>@;:'"+" "*(120-56-1)+"~")
     out_file.close()
+
+
+    out_file = open("18.dev", "w")
+    self.check1(
+      regs = {
+        'W_LOCKED' : set([2000, 2001, 2002]),
+        'RW_LOCKED' : set([3000, 3001, 3002])
+      },
+      # fill zeros memory from 128 to 150
+      memory = dict(
+        {
+          0   : [+1, 2, 0, 0, 18, 37], # out 128(18)
+          151 : [+1, 0, 0, 0, 0, 10],
+        }.items() +
+        [(x, [+1, 0, 0, 0, 0, 0]) for x in xrange(128, 151)]
+      ) ,
+      devs = {
+        18 : (0, 'w', 24*5, 24*2, out_file)
+      },
+      diff = {
+        'CA' : 1,
+        'W_LOCKED' : set([2000, 2001, 2002] + range(128, 152))
+      },
+      cycles = 1
+    )
+    out_file.close()
+    out_file = open("18.dev", "r") 
+    self.assertEqual(out_file.read(), " " * 119 + "~")
+    out_file.close()
+
+    out_file = open("18.dev", "w")
+    self.check_hlt(
+      regs = {
+        'W_LOCKED' : set([2000, 2001, 2002]),
+        'RW_LOCKED' : set([3000, 3001, 3002])
+      },
+      # fill zeros memory from 128 to 150
+      memory = dict(
+        {
+          0   : [+1, 2, 0, 0, 18, 37], # out 128(18)
+          1   : [+1, 0, 1, 0, 18, 34], # jbus *(18)
+          2   : [+1, 0, 0, 0, 18, 35], # ioc (18)
+          3   : [+1, 0, 3, 0, 18, 34], # jbus *(18)
+          4   : [+1, 2, 0, 0, 18, 37], # out 128(18)
+          5   : [+1, 0, 0, 0, 2, 5], # hlt
+          151 : [+1, 0, 0, 0, 0, 10],
+        }.items() +
+        [(x, [+1, 0, 0, 0, 0, 0]) for x in xrange(128, 151)]
+      ) ,
+      devs = {
+        18 : (0, 'w', 24*5, 24*2, out_file)
+      },
+      diff = {
+        'CA' : 6,
+        'J' : [+1, 0, 0, 0, 0, 4],
+        'W_LOCKED' : set([2000, 2001, 2002] + range(128, 152)),
+        'HLT' : 1
+      },
+      cycles = 109
+    )
+    out_file.close()
+    out_file = open("18.dev", "r") 
+    self.assertEqual(out_file.read().splitlines(), [" " * 119 + "~", " " * 119 + "~"] )
+    out_file.close()
+
+
+    out_file = open("18.dev", "w")
+    self.assertRaises(ReadLocked, self.exec_hlt,
+      regs = {
+        'W_LOCKED' : set([2000, 2001, 2002]),
+        'RW_LOCKED' : set([3000, 3001, 3002, 148])
+      },
+      memory = {
+        0   : [+1, 2, 0, 0, 18, 37], # out 128(18)
+        1   : [+1, 0, 1, 0, 18, 34], # jbus *
+        2   : [+1, 0, 0, 0, 2, 5] # hlt
+      },
+      devs = {
+        18 : (0, 'w', 24*5, 24*2, out_file)
+      }
+    )
+
+    out_file = open("18.dev", "w")
+    self.assertRaises(UnsupportedDeviceMode, self.exec_hlt,
+      regs = {
+        'W_LOCKED' : set([2000, 2001, 2002]),
+        'RW_LOCKED' : set([3000, 3001, 3002])
+      },
+      memory = {
+        0   : [+1, 2, 0, 0, 18, 37], # out 128(18)
+        1   : [+1, 0, 1, 0, 18, 34], # jbus *
+        2   : [+1, 0, 0, 0, 2, 5] # hlt
+      },
+      devs = {
+        18 : (0, 'r', 24*5, 24*2, out_file)
+      }
+    )
+
+    out_file = open("18.dev", "w")
+    self.assertRaises(InvalidDevice, self.exec_hlt,
+      regs = {
+        'W_LOCKED' : set([2000, 2001, 2002]),
+        'RW_LOCKED' : set([3000, 3001, 3002])
+      },
+      memory = {
+        0   : [+1, 2, 0, 0, 17, 37], # out 128(18)
+        1   : [+1, 0, 1, 0, 18, 34], # jbus *
+        2   : [+1, 0, 0, 0, 2, 5] # hlt
+      },
+      devs = {
+        18 : (0, 'w', 24*5, 24*2, out_file)
+      }
+    )
+
+    out_file = open("18.dev", "w")
+    self.assertRaises(InvalidCharCode, self.exec_hlt,
+      regs = {
+        'W_LOCKED' : set([2000, 2001, 2002]),
+        'RW_LOCKED' : set([3000, 3001, 3002])
+      },
+      memory = {
+        0   : [+1, 2, 0, 0, 18, 37], # out 128(18)
+        1   : [+1, 0, 1, 0, 18, 34], # jbus *
+        2   : [+1, 0, 0, 0, 2, 5], # hlt
+        128 : [+1, 0, 0, 56, 0, 0]
+      },
+      devs = {
+        18 : (0, 'w', 24*5, 24*2, out_file)
+      }
+    )
+
+    out_file = open("18.dev", "w")
+    self.assertRaises(InvalidAddress, self.exec_hlt,
+      regs = {
+        'W_LOCKED' : set([2000, 2001, 2002]),
+        'RW_LOCKED' : set([3000, 3001, 3002])
+      },
+      memory = {
+        0   : [-1, 2, 0, 0, 18, 37], # out 128(18)
+        1   : [+1, 0, 1, 0, 18, 34], # jbus *
+        2   : [+1, 0, 0, 0, 2, 5] # hlt
+      },
+      devs = {
+        18 : (0, 'w', 24*5, 24*2, out_file)
+      }
+    )
+
+    out_file = open("18.dev", "w")
+    self.assertRaises(IOMemRange, self.exec_hlt,
+      regs = {
+        'W_LOCKED' : set([2000, 2001, 2002]),
+        'RW_LOCKED' : set([3000, 3001, 3002])
+      },
+      memory = {
+        0   : [+1, 62, 30, 0, 18, 37], # out 3998(18)
+        1   : [+1, 0, 1, 0, 18, 34], # jbus *
+        2   : [+1, 0, 0, 0, 2, 5] # hlt
+      },
+      devs = {
+        18 : (0, 'w', 24*5, 24*2, out_file)
+      }
+    )
+
+
 
 suite = unittest.makeSuite(VM3IOTestCase, 'test')
 
