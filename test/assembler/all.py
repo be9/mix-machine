1ed49d7bd9e0cce6ebae6de2f3ff5ffa1214803f
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-08-06 22:54:14 +0700

    asm: listing tests added to all.py

--- test/assembler/all.py
+++ test/assembler/all.py
@@ -11,11 +11,12 @@ import test_memory
 import test_operations
 import test_assemble
 import test_complete_programs
+import test_listing
 
 def suite():
   return unittest.TestSuite((test_parse_line.suite, test_parse_lines.suite, test_symbol_table.suite,
     test_memory.suite, test_operations.suite, test_parse_argument.suite, test_assemble.suite,
-    test_complete_programs.suite))
+    test_complete_programs.suite, test_listing.suite))
 
 if __name__ == "__main__":
   unittest.TextTestRunner().run(suite())
