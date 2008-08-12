e1911f0a30fb64bf9aa4beada0b68aa8ef88f80f
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-08-13 03:55:06 +0700

    gui: mixWordWidget stores word

--- gui/mix_word_widget.py
+++ gui/mix_word_widget.py
@@ -1,26 +1,26 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-from cell_edit import CellEdit
+from word_edit import WordEdit
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'vm2'))
+
+from word import *
 
 class MixWordWidget(QLineEdit):
   def __init__(self, parent = None):
     QLineEdit.__init__(self, "+ 00 00 00 00 00", parent)
     self.setReadOnly(True)
     self.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+    self.word = Word()
+
+  def setWord(self, word):
+    self.word = Word(word)
+    self.setText(str(self.word))
 
   def mouseDoubleClickEvent(self, event): # overload doubleclick event
-    self.emit(SIGNAL("doubleClicked()"))
-
-
-if __name__ == "__main__":
-  def test_double_click():
-    print "Double-clicked!"
-  import sys
-  app = QApplication(sys.argv)
-
-  mix_word = MixWordWidget(None)
-  mix_word.show()
-  app.connect(mix_word, SIGNAL("doubleClicked()"), test_double_click)
-
-  sys.exit(app.exec_())
+    word_edit = WordEdit(self.word, self)
+    if word_edit.exec_():
+      self.setWord(word_edit.word)
+      self.emit(SIGNAL("valueChanged()"))
