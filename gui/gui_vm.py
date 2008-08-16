1e774bf8f48fe0a2fcd2e383f82eae33ca13be36
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-08-17 03:26:50 +0700

    gui: MemoryModel separated separated from vm_data interface

--- gui/gui_vm.py
+++ gui/gui_vm.py
@@ -6,59 +6,6 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'vm2'))
 
 from virt_machine import *
-from word import *
-from vm2_errors import *
-
-class MemoryModel(QAbstractTableModel):
-  def __init__(self, vm_data = None, parent = None):
-    QAbstractTableModel.__init__(self, parent)
-    if vm_data is not None:
-      self.vm_data = vm_data
-      self.memory = vm_data.vm.memory
-      self.mem_len = vm_data.vm.MEMORY_SIZE
-      self.inited = True
-    else:
-      self.inited = False
-
-  def rowCount(self, parent):
-    if self.inited:
-      return self.mem_len
-    else:
-      return 0
-
-  def columnCount(self, parent):
-    return 1 # word
-
-  def data(self, index, role = Qt.DisplayRole):
-    if not index.isValid():
-      return QVariant()
-
-    if role == Qt.TextAlignmentRole:
-      return QVariant(Qt.AlignHCenter | Qt.AlignVCenter)
-
-    elif role == Qt.DisplayRole:
-      if self.vm_data.is_readable(index.row()):
-        return QVariant(  word2str(     self.memory[index.row()]  ))
-      else:
-        return QVariant(self.tr("LOCKED"))
-    elif role == Qt.ToolTipRole:
-      return QVariant(  word2toolTip( self.memory[index.row()]  ))
-
-    else:
-      return QVariant()
-
-  def headerData(self, section, orientation, role = Qt.DisplayRole):
-    if role == Qt.TextAlignmentRole:
-      return QVariant(Qt.AlignHCenter | Qt.AlignVCenter)
-
-    elif role == Qt.DisplayRole:
-      if orientation == Qt.Horizontal:
-        return QVariant(self.tr("Mix word"))
-      else:
-        return QVariant(str(section))
-
-    else:
-      return QVariant()
 
 class VMData:
   def __init__(self, asm_data):
