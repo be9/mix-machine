76f585ce1f21bad40b606e18a56819d9b6c08ef2
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-08-22 21:49:11 +0700

    gui: main module totaly cleaned
    not working program now :)

--- gui/main.py
+++ gui/main.py
@@ -43,7 +43,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     self.file_filters = self.tr("MIX source files (*.mix);;All files (*.*)");
     self.setCurrentFile("")
-
     self.txt_source.setPlainText(u"")
 
     self.connect(self.action_Quit, SIGNAL("triggered()"), qApp, SLOT("closeAllWindows()"))
@@ -62,7 +61,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     self.connect(self.action_Change_font, SIGNAL("triggered()"), self.slot_Change_font)
 
-
     about_text = self.tr("""
     <h2>Mix Machine</h2><br><br>
     An implementation of Don Knuth's MIX machine in Python with GUI<br>
@@ -76,23 +74,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     self.connect(self.tabWidget, SIGNAL("currentChanged(int)"), self.slot_cur_tab_changed)
 
-    self.setRunWidgetsEnabled(False)
-    self.errors_list.setVisible(False)
-
-    # init listing and disasmmler view
-    self.listing_model = ListingModel(parent = self)
-    self.listing_view.setModel(self.listing_model) # add model to set size of header
-    self.listing_view.horizontalHeader().setStretchLastSection(True) # for column with source line
-    self.listing_view.setSelectionMode(QAbstractItemView.NoSelection)
-
-    self.disasm_model = DisassemblerModel(parent = self)
-    self.disasm_view.setModel(self.disasm_model) # add model to set size of header
-    self.disasm_view.horizontalHeader().setStretchLastSection(True) # for column with disasm line
-    self.disasm_view.setSelectionMode(QAbstractItemView.NoSelection)
-
-    self.resetSizes()
-
-    self.slot_cur_tab_changed(0)
+    self.emit(SIGNAL("inited()"))
 
     self.output_device = QTextEditOutputDevice(
         mode = "w", block_size = 24 * 5, lock_time = 24*2, text_edit = self.dev_dock.text_printer
@@ -102,57 +84,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     )
 
   def slot_cur_tab_changed(self, index):
-    visible = index != 0 # hide dock widgets at source editing tab
-    self.mem_dock.setVisible(visible)
-    self.cpu_dock.setVisible(visible)
-    self.dev_dock.setVisible(visible)
-    if visible:
-      self.listing_and_disasm_goto_ca()
-
-  def resetSizes(self):
-    """Call after font changes"""
-    font_metrics = QFontMetrics(self.listing_view.font())
-    addr = font_metrics.width("0000")
-    word = font_metrics.width("+ 00 00 00 00 00")
-
-    h_header = self.listing_view.horizontalHeader()
-    h_header.setStretchLastSection(True) # for column with source line
-    h_header.resizeSection(h_header.logicalIndex(0), addr + 20)
-    h_header.resizeSection(h_header.logicalIndex(1), word + 20)
-
-    h_header = self.disasm_view.horizontalHeader()
-    h_header.setStretchLastSection(True) # for column with disasm line
-    h_header.resizeSection(h_header.logicalIndex(0), addr + 20)
-    h_header.resizeSection(h_header.logicalIndex(1), word + 20)
+    if index == 0:
+      self.emit(SIGNAL("sourceTabFocused()"))
+    else:
+      self.emit(SIGNAL("traceTabFocused()"))
 
   def slot_Change_font(self):
     new_font, ok = QFontDialog.getFont(self.txt_source.font())
     if ok:
-      self.txt_source.setFont(new_font)
-      self.listing_view.setFont(new_font)
-      self.resetSizes()
-
-  def setRunWidgetsEnabled(self, enable):
-    self.tabWidget.setTabEnabled(1, enable)
-    self.tabWidget.setTabEnabled(2, enable)
-    self.action_Step.setEnabled(enable)
-    self.action_Trace.setEnabled(enable)
-    self.action_Run.setEnabled(enable)
-    self.action_Break.setEnabled(enable)
-
-  def setBreakEnabledOnly(self, enable):
-    self.menu_File.setEnabled(not enable)
-    self.menu_Options.setEnabled(not enable)
-    self.action_Assemble.setEnabled(not enable)
-    self.action_Step.setEnabled(not enable)
-    self.action_Trace.setEnabled(not enable)
-    self.action_Run.setEnabled(not enable)
-    self.action_Break.setEnabled(enable)
-
-  def setNewSource(self):
-    self.setRunWidgetsEnabled(False)
-    self.tabWidget.setCurrentIndex(0)
-    self.errors_list.setVisible(False)
+      self.emit(SIGNAL("fontChanged(QFont)"), new_font)
 
   def slot_File_New(self):
     if not self.checkUnsaved(): 
@@ -164,7 +104,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     self.setCurrentFile("")
 
-    self.setNewSource()
+    self.emit(SIGNAL("setNewSource()"))
 
     self.statusBar().showMessage(self.tr("Empty source file has been created."), 2000);
 
@@ -216,7 +156,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setCurrentFile(fn)
 
   def closeEvent(self, ce):
-    self.slot_Break()
+    self.slot_Break() # break machine if running
     if self.checkUnsaved():
       ce.accept()
     else:
@@ -261,7 +201,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
       self.statusBar().showMessage(self.tr("Error loading file."), 2000)
 
     else:
-      self.setNewSource()
+      self.emit(SIGNAL("setNewSource()"))
 
       self.setCurrentFile(filename)
 
@@ -285,7 +225,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     self.listing_model.hook(item, old, new)
     self.disasm_model.hook(item, old, new)
     if item == "cur_addr":
-      self.listing_and_disasm_goto_ca()
+      self.emit(SIGNAL("caChanged(int)"), new)
 
   def mem_hook(self, addr, old, new):
     self.mem_dock.hook(addr, old, new)
@@ -297,18 +237,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     self.listing_model.hook(mode, old, new)
     self.disasm_model.hook(mode, old, new)
 
-  def resetListingAndDisasm(self):
-    self.listing_model = ListingModel(vm_data = self.vm_data, parent = self)
-    self.listing_view.setModel(self.listing_model)
-
-    self.disasm_model = DisassemblerModel(vm_data = self.vm_data, asm_data = self.asm_data, parent = self)
-    self.disasm_view.setModel(self.disasm_model)
-
-    self.listing_and_disasm_goto_ca()
-
   def slot_Assemble(self):
-    self.errors_list.setVisible(False)
-    self.setRunWidgetsEnabled(False)
+    self.emit(SIGNAL("beforeAssemble()"))
+
     ret_type, content = asm(unicode(self.txt_source.toPlainText()))
     if ret_type == ASM_NO_ERRORS:
       self.asm_data = content # mem, start_addr, listing
@@ -321,32 +252,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
       self.input_device.reset()
       self.vm_data.addDevice(19, self.input_device)
 
-      self.mem_dock.initModel(self.vm_data)
-
-      self.cpu_dock.setVMData(self.vm_data)
-      self.cpu_dock.loadFromVM()
-      self.cpu_dock.resetHighlight()
-
-      self.resetListingAndDisasm()
-
-      self.setRunWidgetsEnabled(True)
-      self.setBreakEnabledOnly(False)
-      self.tabWidget.setCurrentIndex(1)
-      self.statusBar().showMessage(self.tr("Source assembled and virtual machine initialized"), 2000)
+      self.emit(SIGNAL("assembleSuccess(VMData)"), self.vm_data)
       return
 
-    # we have errors!
-    if ret_type == ASM_SYNTAX_ERRORS:
-      err_mesg = self.tr("There are syntax errors")
-    else:
-      err_mesg = self.tr("There are assemble errors")
-
-    # content - errors
-    self.errors_list.clear()
-    self.errors_list.addItems([ "%i: %s" % err for err in content ])
-    self.errors_list.setVisible(True)
-
-    self.statusBar().showMessage(err_mesg, 2000)
+    # we have errors! (emit type of errors and list of errors)
+    self.emit(SIGNAL("assembleGotErrors(int, QStringList)"), ret_type, [ "%i: %s" % err for err in content ])
 
   def enableHooks(self, enable = True):
     if enable:
@@ -358,45 +268,50 @@ class MainWindow(QMainWindow, Ui_MainWindow):
       self.vm_data.setMemHook(None)
       self.vm_data.setLockHook(None)
 
-  def doVMAction(self, action):
-    self.breaked = False
+  def start_action(self):
+    self.running = True
+    #self.emit(SIGNAL("startedAction()")) # not used
+
+  def stop_action(self):
+    self.running = False
+    self.emit(SIGNAL("stoppedAction()"))
+
+  def doAction(self, action):
     if self.vm_data.halted():
       QMessageBox.information(self, self.tr("Mix machine"), self.tr("Mix machine is halted."))
       return
 
-    self.cpu_dock.resetHighlight()
-    self.setBreakEnabledOnly(True)
     try:
+      self.start_action()
       action()
     except Exception, err:
-      QMessageBox.critical(self, self.tr("Runtime error"), str(err))
+      self.stop_action()
+      if err in self.vm_data.vm_errors:
+        QMessageBox.critical(self, self.tr("Runtime error"), str(err))
+      else:
+        raise err
     else:
-      self.listing_and_disasm_goto_ca()
+      self.stop_action()
       if self.vm_data.halted():
         QMessageBox.information(self, self.tr("Mix machine"), self.tr("Mix machine was halted."))
-    self.setBreakEnabledOnly(False)
 
   def slot_Step(self):
-    self.enableHooks()
-    self.doVMAction(self.vm_data.step)
+    self.emit(SIGNAL("beforeTrace()"))
+    self.doAction(self.vm_data.step)
+    self.emit(SIGNAL("afterTrace()"))
 
   def run_vm(self):
-    while not self.vm_data.halted() and not self.breaked:
+    while not self.vm_data.halted() and self.running:
       self.vm_data.step()
       QCoreApplication.processEvents()
 
   def slot_Trace(self):
-    self.enableHooks()
-    self.doVMAction(self.run_vm)
-
-  def enableAllWidgetsButDev(self, enable):
-    self.tabWidget.setEnabled(enable)
-    self.cpu_dock.setEnabled(enable)
-    self.mem_dock.setEnabled(enable)
+    self.emit(SIGNAL("beforeTrace()"))
+    self.doAction(self.run_vm)
+    self.emit(SIGNAL("afterTrace()"))
 
   def slot_Run(self):
-    self.enableHooks(False)
-    self.enableAllWidgetsButDev(False)
+    self.emit(SIGNAL("beforeRun()"))
 
     self.progress = QProgressDialog(self.tr("Running (%1 cycles passed)").arg(0), self.tr("Break run"), 0, 10, self)
     self.progress.setMinimumDuration(0)
@@ -406,6 +321,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     self.progress_timer = QTimer(self)
     self.connect(self.progress_timer, SIGNAL("timeout()"), self.progressTick)
+    self.connect(self, SIGNAL("stoppedAction()"), self.progress_timer, SLOT("stop()"))
     self.progress_timer.start(100)
 
     self.progress.setValue(0)
@@ -415,9 +331,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     self.progress.cancel()
     del self.progress, self.progress_timer
 
-    self.enableAllWidgetsButDev(True)
-    self.cpu_dock.loadFromVM()
-    self.resetListingAndDisasm()
+    self.emit(SIGNAL("afterRun()"))
 
   def progressTick(self):
     self.progress.setLabelText(self.tr("Running (%1 cycles passed)").arg(self.vm_data.cycles()))
@@ -426,19 +340,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     else:
       self.progress.setValue(self.progress.value() + 1)
 
-  def listing_and_disasm_goto_ca(self):
-    """Selects row near ca"""
-    if self.tabWidget.currentIndex() == 1: # listing
-      num = self.listing_model.current_line
-      if num is None:
-          self.statusBar().showMessage(self.tr("Mix machine's current address is out of listing."))
-      else:
-        self.listing_view.setCurrentIndex(self.listing_model.index(num, 0))
-    elif self.tabWidget.currentIndex() == 2: # disasm
-      self.disasm_view.setCurrentIndex(self.disasm_model.index(self.disasm_model.ca, 0))
-
   def slot_Break(self):
-    self.breaked = True
+    self.emit(SIGNAL("breaked()"))
+    self.running = False
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
