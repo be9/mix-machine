5a971b9b40f97e3fec830cd225e6f412df89f808
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-08-28 01:17:15 +0700

    gui: now cpu_dock nicer

--- gui/dock_cpu.py
+++ gui/dock_cpu.py
@@ -11,8 +11,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'vm2'))
 
 from word import *
 
-registers = "a x i1 i2 i3 i4 i5 i6 j".split()
-flags = "ca cf of hlt cycles".split()
+registers = "ax123456j"
+flags = "cur_addr cf of halted cycles".split()
+label_flags = "CA CF OF HLT CyCLES".split()
 
 HIGHLIGHT_STYLE = "MixWordWidget, QComboBox { background: gray }"
 
@@ -23,21 +24,16 @@ class CPUDockWidget(QDockWidget):
     self.initConnections()
 
   def init(self, vm_data):
-    self.edit_ca.setMaximum(vm_data.vm.MEMORY_SIZE - 1)
+    self.edit_cur_addr.setMaximum(vm_data.vm.MEMORY_SIZE - 1)
     self.vm_data = vm_data
     self.reload()
     self.resetHighlight()
 
   def reload(self):
-    for s in "AXJ":
+    for s in registers.upper():
       self.setItem(s, self.vm_data.vm[s])
-    for s in "123456":
+    for s in flags:
       self.setItem(s, self.vm_data.vm[s])
-    self.setItem("cur_addr",  self.vm_data.vm["cur_addr"])
-    self.setItem("cf",        self.vm_data.vm["cf"])
-    self.setItem("of",        self.vm_data.vm["of"])
-    self.setItem("halted",    self.vm_data.vm["halted"])
-    self.setItem("cycles",    self.vm_data.vm["cycles"])
 
   def highlight(self, object, flag = True):
     if not flag:
@@ -49,24 +45,17 @@ class CPUDockWidget(QDockWidget):
     map(lambda obj: self.highlight(obj, False), self.all_edits)
 
   def setItem(self, item, value):
+    item = item.lower()
     if item == "cur_addr":
-      self.edit_ca.setValue(value)
-
-    elif item in "AXJ":
+      self.edit_cur_addr.setValue(value)
+    elif item in registers:
       self.__dict__["edit_" + item.lower()].setWord(value)
-
-    elif item in "123456":
-      self.__dict__["edit_i" + item].setWord(value)
-
     elif item == "cf":
       self.edit_cf.setCurrentIndex(value + 1)
-
     elif item == "of":
       self.edit_of.setCurrentIndex(int(value))
-
     elif item == "halted":
-      self.edit_hlt.setCurrentIndex(int(value))
-
+      self.edit_halted.setCurrentIndex(int(value))
     elif item == "cycles":
       self.edit_cycles.display(value)
 
@@ -74,30 +63,18 @@ class CPUDockWidget(QDockWidget):
     self.setItem(item, new)
 
   def setVM(self, what, value = None):
-    # what - in lower
-    if what in "axj":
+    what = what.lower()
+    if what in registers:
       if value is None:
         self.vm_data.vm[what.upper()] = Word(self.__dict__["edit_"+what].word)
       else:
         self.vm_data.vm[what.upper()] = Word(value)
-
-    elif what in "i1 i2 i3 i4 i5 i6".split():
-      if value is None:
-        self.vm_data.vm[what[1]] = Word(self.__dict__["edit_"+what].word)
-      else:
-        self.vm_data.vm[what[1]] = Word(value)
-
-    elif what == "ca":
+    elif what == "cur_addr":
       self.vm_data.vm["cur_addr"] = value
-
     elif what == "cf":
       self.vm_data.vm["cf"] = value - 1
-
-    elif what == "of":
-      self.vm_data.vm["of"] = bool(value)
-
-    elif what == "hlt":
-      self.vm_data.vm["halted"] = bool(value)
+    elif what in ("of", "halted"):
+      self.vm_data.vm[what] = bool(value)
 
   def slot_cycles_overflow(self):
     # log10(cycles.value()) rounded up - this is number of digits :) (learn math)
@@ -105,32 +82,32 @@ class CPUDockWidget(QDockWidget):
     self.edit_cycles.display(self.edit_cycles.intValue())
 
   def initConnections(self):
-    self.connect(self.edit_a, SIGNAL("valueChanged()"),             lambda: self.setVM("a"))
-    self.connect(self.edit_x, SIGNAL("valueChanged()"),             lambda: self.setVM("x"))
-    self.connect(self.edit_j, SIGNAL("valueChanged()"),             lambda: self.setVM("j"))
-    self.connect(self.edit_i1, SIGNAL("valueChanged()"),            lambda: self.setVM("i1"))
-    self.connect(self.edit_i2, SIGNAL("valueChanged()"),            lambda: self.setVM("i2"))
-    self.connect(self.edit_i3, SIGNAL("valueChanged()"),            lambda: self.setVM("i3"))
-    self.connect(self.edit_i4, SIGNAL("valueChanged()"),            lambda: self.setVM("i4"))
-    self.connect(self.edit_i5, SIGNAL("valueChanged()"),            lambda: self.setVM("i5"))
-    self.connect(self.edit_i6, SIGNAL("valueChanged()"),            lambda: self.setVM("i6"))
-    self.connect(self.edit_ca, SIGNAL("valueChanged(int)"),         lambda i: self.setVM("ca", i))
-    self.connect(self.edit_cf, SIGNAL("currentIndexChanged(int)"),  lambda index: self.setVM("cf", index))
-    self.connect(self.edit_of, SIGNAL("currentIndexChanged(int)"),  lambda index: self.setVM("of", index))
-    self.connect(self.edit_hlt, SIGNAL("currentIndexChanged(int)"), lambda index: self.setVM("hlt", index))
+    self.connect(self.edit_a, SIGNAL("valueChanged()"),                 lambda: self.setVM("a"))
+    self.connect(self.edit_x, SIGNAL("valueChanged()"),                 lambda: self.setVM("x"))
+    self.connect(self.edit_j, SIGNAL("valueChanged()"),                 lambda: self.setVM("j"))
+    self.connect(self.edit_1, SIGNAL("valueChanged()"),                 lambda: self.setVM("1"))
+    self.connect(self.edit_2, SIGNAL("valueChanged()"),                 lambda: self.setVM("2"))
+    self.connect(self.edit_3, SIGNAL("valueChanged()"),                 lambda: self.setVM("3"))
+    self.connect(self.edit_4, SIGNAL("valueChanged()"),                 lambda: self.setVM("4"))
+    self.connect(self.edit_5, SIGNAL("valueChanged()"),                 lambda: self.setVM("5"))
+    self.connect(self.edit_6, SIGNAL("valueChanged()"),                 lambda: self.setVM("6"))
+    self.connect(self.edit_cur_addr, SIGNAL("valueChanged(int)"),       lambda i: self.setVM("cur_addr", i))
+    self.connect(self.edit_cf, SIGNAL("currentIndexChanged(int)"),      lambda index: self.setVM("cf", index))
+    self.connect(self.edit_of, SIGNAL("currentIndexChanged(int)"),      lambda index: self.setVM("of", index))
+    self.connect(self.edit_halted, SIGNAL("currentIndexChanged(int)"),  lambda index: self.setVM("hlt", index))
 
-    self.connect(self.edit_a, SIGNAL("valueChanged()"),             lambda: self.highlight(self.edit_a))
-    self.connect(self.edit_x, SIGNAL("valueChanged()"),             lambda: self.highlight(self.edit_x))
-    self.connect(self.edit_j, SIGNAL("valueChanged()"),             lambda: self.highlight(self.edit_j))
-    self.connect(self.edit_i1, SIGNAL("valueChanged()"),            lambda: self.highlight(self.edit_i1))
-    self.connect(self.edit_i2, SIGNAL("valueChanged()"),            lambda: self.highlight(self.edit_i2))
-    self.connect(self.edit_i3, SIGNAL("valueChanged()"),            lambda: self.highlight(self.edit_i3))
-    self.connect(self.edit_i4, SIGNAL("valueChanged()"),            lambda: self.highlight(self.edit_i4))
-    self.connect(self.edit_i5, SIGNAL("valueChanged()"),            lambda: self.highlight(self.edit_i5))
-    self.connect(self.edit_i6, SIGNAL("valueChanged()"),            lambda: self.highlight(self.edit_i6))
-    self.connect(self.edit_cf, SIGNAL("currentIndexChanged(int)"),  lambda: self.highlight(self.edit_cf))
-    self.connect(self.edit_of, SIGNAL("currentIndexChanged(int)"),  lambda: self.highlight(self.edit_of))
-    self.connect(self.edit_hlt, SIGNAL("currentIndexChanged(int)"), lambda: self.highlight(self.edit_hlt))
+    self.connect(self.edit_a, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_a))
+    self.connect(self.edit_x, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_x))
+    self.connect(self.edit_j, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_j))
+    self.connect(self.edit_1, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_1))
+    self.connect(self.edit_2, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_2))
+    self.connect(self.edit_3, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_3))
+    self.connect(self.edit_4, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_4))
+    self.connect(self.edit_5, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_5))
+    self.connect(self.edit_6, SIGNAL("valueChanged()"),                 lambda: self.highlight(self.edit_6))
+    self.connect(self.edit_cf, SIGNAL("currentIndexChanged(int)"),      lambda: self.highlight(self.edit_cf))
+    self.connect(self.edit_of, SIGNAL("currentIndexChanged(int)"),      lambda: self.highlight(self.edit_of))
+    self.connect(self.edit_halted, SIGNAL("currentIndexChanged(int)"),  lambda: self.highlight(self.edit_halted))
 
   def initWidgets(self):
     self.setWindowTitle(self.tr("CPU"))
@@ -164,15 +141,15 @@ class CPUDockWidget(QDockWidget):
 
     # create all flags:
     # first create all labels
-    for flag in flags:
-      label = self.__dict__["label_" + flag]   = QLabel(self.tr(flag.upper()), self)
-      label.setObjectName("label_" + flag)
+    for i in xrange(len(flags)):
+      label = self.__dict__["label_" + flags[i]]   = QLabel(self.tr(label_flags[i]), self)
+      label.setObjectName("label_" + flags[i])
       label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
 
     # than create edit-widgets
-    self.edit_ca = QSpinBox(self)
-    self.edit_ca.setObjectName("edit_ca")
-    self.edit_ca.setMinimum(0)
+    self.edit_cur_addr = QSpinBox(self)
+    self.edit_cur_addr.setObjectName("edit_cur_addr")
+    self.edit_cur_addr.setMinimum(0)
     #self.edit_ca.setMaximum(0) # it will be set in setVMData
     #self.all_edits.append(self.edit_ca) # no highlight
 
@@ -195,14 +172,14 @@ class CPUDockWidget(QDockWidget):
     self.edit_of.setCurrentIndex(0)
     self.all_edits.append(self.edit_of)
 
-    self.edit_hlt = QComboBox(self)
-    self.edit_hlt.setObjectName("edit_hlt")
-    self.edit_hlt.addItems([
+    self.edit_halted = QComboBox(self)
+    self.edit_halted.setObjectName("edit_halted")
+    self.edit_halted.addItems([
       self.tr("False"),
       self.tr("True")
     ])
-    self.edit_hlt.setCurrentIndex(0)
-    self.all_edits.append(self.edit_hlt)
+    self.edit_halted.setCurrentIndex(0)
+    self.all_edits.append(self.edit_halted)
 
     self.edit_cycles = QLCDNumber(self)
     self.edit_cycles.setObjectName("edit_cycles")
@@ -215,7 +192,7 @@ class CPUDockWidget(QDockWidget):
     self.main_layout = QGridLayout()
 
     # add all flags to main_layout
-    for s in registers + flags:
+    for s in list(registers) + flags:
       self.main_layout.addWidget(self.__dict__["label_" + s],  current_row_in_grid, 0, 1, 1)
       self.main_layout.addWidget(self.__dict__["edit_" + s],   current_row_in_grid, 1, 1, 3)
       current_row_in_grid += 1
