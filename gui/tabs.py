b30ebdf80a678e833b549bc5de5dd0a08e320701
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-08-27 19:00:38 +0700

    gui: tabs now remember, what user prefer (listing or disasm)

--- gui/tabs.py
+++ gui/tabs.py
@@ -4,14 +4,17 @@ from PyQt4.QtGui import *
 class MixTabs(QTabWidget):
   def __init__(self, parent = None):
     QTabWidget.__init__(self, parent)
+    self.lastRunIndex = 1 # index of last used tab (1 - listing, 2 - disassembler)
 
   def showRun(self):
     self.setTabEnabled(1, True)
     self.setTabEnabled(2, True)
-    if self.currentIndex() == 0:
-      self.setCurrentIndex(1)
+    self.setCurrentIndex(self.lastRunIndex)
 
   def hideRun(self):
     self.setTabEnabled(1, False)
     self.setTabEnabled(2, False)
     self.setCurrentIndex(0)
+
+  def rememberRunTab(self):
+    self.lastRunIndex = self.currentIndex()
